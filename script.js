0670826d981044bed3021b5bca3dca0d3389ecbf
Gustavo <gamoriim.io@gmail.com>
2025-06-11 20:56:00 -0300

    added step 3: hover effect when you passes over them

--- script.js
+++ script.js
@@ -4,4 +4,12 @@ for(let i = 0; i < 256; i++) {
     const cell = document.createElement("div");
     cell.classList.add("cell");
     div.appendChild(cell)
+    
+    // charge the color
+    cell.addEventListener("mouseenter", ()=> {
+    cell.classList.add("markdown");
+})
+    div.appendChild(cell);
 }
+
+
