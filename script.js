a381e1b862701ecebcc55501dfcdf0a62759998a
Gustavo <gamoriim.io@gmail.com>
2025-06-12 20:19:29 -0300

    added the button

--- script.js
+++ script.js
@@ -12,4 +12,27 @@ for(let i = 0; i < 256; i++) {
     div.appendChild(cell);
 }
 
+const btn = document.querySelector("#btn");
 
+btn.addEventListener("click", ()=> {
+    const input = prompt("Type the number of squares: ");
+    const value = parseInt(input);
+    console.log(value)
+
+    if (isNaN(value) || value <1 || value > 100) {
+        alert("Invalid number, please enter a number between 1 and 100");
+    } else {
+        div.innerHTML = "";
+        for(let i = 0; i < value; i++) {
+    const cell = document.createElement("div");
+    cell.classList.add("cell");
+    
+    // charge the color
+    cell.addEventListener("mouseenter", ()=> {
+    cell.classList.add("markdown");
+})
+    div.appendChild(cell);
+}
+
+    }
+})
